4fcf1fb539e6d17d5053b52c61f47b3ffdb8ac3e
Niels van der Meer <210833@talnet.nl>
2019-11-25 15:07:01 +0100

    opdracht 3 af

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,17 @@
-//javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if (saldo==500) {
+        document.getElementById("tekst").innerHTML = "Saldo is gestort zonder bonus.";
+    }
+
+    else if (saldo<500) {
+        document.getElementById("tekst").innerHTML = "Saldo is niet gestort";
+    }
+
+    else {
+        document.getElementById("tekst").innerHTML = "Saldo is gestort met bonus.";
+    }
+
+
+}

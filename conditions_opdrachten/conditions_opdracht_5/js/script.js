81d44eec68bf59f8f5ef088cf1d7e42f4079a095
Niels van der Meer <210833@talnet.nl>
2019-11-26 13:12:56 +0100

    nieuwe opdracht 5 af

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -2,7 +2,7 @@ calculate = function(keer)
 {
     var getal1 = document.getElementById('a1').value;
     var getal2 = document.getElementById('a2').value; 
-    document.getElementById('a3').value = parseInt(getal1) * parseInt(getal2);
+    document.getElementById('a3').value = parseInt(getal1) + parseInt(getal2);
      
    }
 
@@ -10,6 +10,6 @@ calculate2 = function(delen)
    {
        var getal3 = document.getElementById('b1').value;
        var getal4 = document.getElementById('b2').value; 
-       document.getElementById('b3').value = parseInt(getal3) / parseInt(getal4);
+       document.getElementById('b3').value = parseInt(getal3) - parseInt(getal4);
         
       }
